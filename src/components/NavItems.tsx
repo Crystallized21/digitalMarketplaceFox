6a160b584a1855da2b26543d632a6f3527eabffa
Michael Bui <michael.bui_crystal@outlook.com>
2024-07-23 14:24:52 +1200

    built navbar layout

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,17 +1,41 @@
 "use client"
 
-import {useState} from "react";
+import React, {useState} from "react";
+import {PRODUCT_CATEGORIES} from "@/config";
+import NavItem from "@/components/NavItem";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<
         null | number
     >(null)
 
-    return (
-        <div className="flex gap-4 h-full">
+    const isAnyOpen = activeIndex !== null;
 
+    return (
+        <div className='flex gap-4 h-full'>
+            {PRODUCT_CATEGORIES.map((category, i) => {
+                const handleOpen = () => {
+                    if (activeIndex === i) {
+                        setActiveIndex(null)
+                    } else {
+                        setActiveIndex(i)
+                    }
+                }
+
+                const isOpen = i === activeIndex
+
+                return (
+                    <NavItem
+                        category={category}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        key={category.value}
+                        isAnyOpen={isAnyOpen}
+                    />
+                )
+            })}
         </div>
-    );
+    )
 };
 
 export default NavItems;
