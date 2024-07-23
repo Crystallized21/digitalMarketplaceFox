9789c217511ee200fd71f17c789c6c932c1e2135
Michael Bui <michael.bui_crystal@outlook.com>
2024-07-23 14:03:37 +1200

    add public images

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,13 @@
 export const PRODUCT_CATERGORIES = [
-
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "public/nav/ui-kits/mixed.jpg"
+            }
+        ]
+    }
 ]
