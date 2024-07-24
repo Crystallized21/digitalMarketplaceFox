21d06b68940994fe85bc5bb4fee5d4ac9518b763
Michael Bui <michael.bui_crystal@outlook.com>
2024-07-24 15:57:02 +1200

    finished navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {PRODUCT_CATEGORIES} from "@/config";
 import NavItem from "@/components/NavItem";
 
 const NavItems = () => {
+    // State to track the currently active (opened) navigation item by index, null when none are active
     const [activeIndex, setActiveIndex] = useState<
         null | number
     >(null)
 
+    // Determine if any navigation item is open
     const isAnyOpen = activeIndex !== null;
+
+    const navRef = useRef<HTMLDivElement | null>()
 
     return (
         <div className='flex gap-4 h-full'>
             {PRODUCT_CATEGORIES.map((category, i) => {
+                // Function to handle opening/closing of navigation items
                 const handleOpen = () => {
                     if (activeIndex === i) {
                         setActiveIndex(null)
@@ -22,8 +27,10 @@ const NavItems = () => {
                     }
                 }
 
+                // Determine if the current item is open based on activeIndex
                 const isOpen = i === activeIndex
 
+                // Render the NavItem component for each category
                 return (
                     <NavItem
                         category={category}
