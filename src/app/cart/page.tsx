a0d911ecae361446034c4de8bc63fb22e3a0cd19
Michael Bui <michael.bui_crystal@outlook.com>
2024-08-11 21:40:36 +1200

    finished shopping cart page, making api for transactions

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { PRODUCT_CATEGORIES } from "@/config";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Check, X } from "lucide-react";
+import { Check, Loader2, X } from "lucide-react";
 
 const Page = () => {
 	const {items, removeItem}  = useCart()
@@ -16,6 +16,13 @@ const Page = () => {
 	useEffect(() => {
 		setIsMounted(true)
 	}, []);
+
+	const cartTotal = items.reduce(
+		(total, { product }) => total + product.price,
+		0
+	)
+
+	const fee = 1
 
 	return (
 		<div className="bg-white">
@@ -53,12 +60,12 @@ const Page = () => {
 							"divide-y divide-gray-200 border-b border-t border-gray-200":
 								isMounted && items.length > 0,
 						})}>
-							{isMounted && items.map(({product}) => {
+							{isMounted && items.map(({ product }) => {
 								const label = PRODUCT_CATEGORIES.find(
 									(c) => c.value === product.category
 								)?.label
 
-								const {image} = product.images[0]
+								const { image } = product.images[0]
 
 								return (
 									<li key={product.id} className="flex py-6 sm:py-10">
@@ -119,7 +126,7 @@ const Page = () => {
 											</div>
 
 											<p className="mt-4 flex space-x-2 text-sm text-gray-700">
-												<Check className="h-5 w-5 flex-shrink-0 text-green-500"/>
+												<Check className="h-5 w-5 flex-shrink-0 text-green-500" />
 
 												<span>
 													Eligible for instant delivery
@@ -131,6 +138,57 @@ const Page = () => {
 							})}
 						</ul>
 					</div>
+
+					<section className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+						<h2 className="text-lg font-medium text-gray-900">
+							Order Summary
+						</h2>
+
+						<div className="mt-6 space-y-4">
+							<div className="flex items-center justify-between">
+								<p className="text-sm text-gray-600">
+									Subtotal
+								</p>
+								<p className="text-sm font-medium text-gray-900">
+									{isMounted ? (
+										formatPrice(cartTotal)
+									) : (
+										<Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+									)}
+								</p>
+							</div>
+
+							<div className="flex items-center justify-between border-t border-gray-200 pt-4">
+								<div className="flex items-center text-sm text-muted-foreground">
+									<span>Flat Transaction Fee</span>
+								</div>
+								<div className="text-sm font-medium text-gray-900">
+									{isMounted ? (
+										formatPrice(fee)
+									) : (
+										<Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+									)}
+								</div>
+							</div>
+
+							<div className="flex items-center justify-between border-t border-gray-200 pt-4">
+								<div className="text-base font-medium text-gray-900">
+									Order Total
+								</div>
+								<div className="text-base font-medium text-gray-900">
+									{isMounted ? (
+										formatPrice(cartTotal + fee)
+									) : (
+										<Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+									)}
+								</div>
+							</div>
+						</div>
+
+						<div className="mt-6">
+							<Button className="w-full" size="lg">Checkout</Button>
+						</div>
+					</section>
 				</div>
 			</div>
 		</div>
