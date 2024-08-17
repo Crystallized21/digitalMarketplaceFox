ea51cc2c680a78a6e8edb909285a8ba64916a29c
Michael Bui <michael.bui_crystal@outlook.com>
2024-08-17 13:32:27 +1200

    debugging

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,15 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === "GET") {
 		const user = {
 			id: 1,
 			name: "John Doe",
-			email: "john.doe@example.com",
+			email: "john.doe@example.com"
 		};
 
 		res.status(200).json({ user });
 	} else {
 		res.status(405).json({ message: "Method Not Allowed" });
 	}
-}
+};
+
+export { handler as GET };
