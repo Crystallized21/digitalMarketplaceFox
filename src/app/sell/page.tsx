2d9351184ac41ebaaf31a031a077e2f14af1c185
Michael Bui <michael.bui_crystal@outlook.com>
2024-08-17 13:52:54 +1200

    debugging

---
+++ src/app/sell/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SellPage = () => {
+    return (
+        <div>
+            <h1>Sell Your Products</h1>
+            <p>Welcome to the sell page. Here you can list your products for sale.</p>
+        </div>
+    );
+};
+
+export default SellPage;
