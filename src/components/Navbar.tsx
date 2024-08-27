fa3d58c1c31f4fe92f0179e96cd862443dead6cf
Michael Bui <michael.bui_crystal@outlook.com>
2024-08-27 18:51:01 +1200

    made MobileNav.tsx, updated middleware.ts, and made a footer and final small tweaks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import Cart from "@/components/Cart";
 import {getServerSideUser} from "@/lib/payload-utils";
 import {cookies} from "next/headers";
 import UserAccountNav from "@/components/UserAccountNav";
+import MobileNav from "@/components/MobileNav";
 
 const Navbar = async () => {
 
@@ -20,7 +21,7 @@ const Navbar = async () => {
                 <MaxWidthWrapper>
                     <div className="border-b border-gray-200">
                         <div className="flex h-16 items-center">
-                            {/* TODO: mobile nav */}
+                            <MobileNav/>
                             <div className="ml-4 flex lg:ml-0">
                                 <Link href="/">
                                     <Icons.logo className="h-10 w-10"/>
